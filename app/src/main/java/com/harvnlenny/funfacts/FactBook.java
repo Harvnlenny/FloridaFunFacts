415f7c8923886909d7fe1c03d4208460529337a9
Aaron Harvey <harvnlenny@gmail.com>
2016-03-24 10:32:37 -0400

    Initial commit

---
+++ app/src/main/java/com/harvnlenny/funfacts/FactBook.java
@@ -0,0 +1,38 @@
+package com.harvnlenny.funfacts;
+
+import java.util.Random;
+
+/**
+ * Created by aaronharvey on 11/10/15.
+ */
+public class FactBook {
+
+    public String[] mFacts = {
+            "Ants stretch when they wake up in the morning.",
+            "Ostriches can run faster than horses.",
+            "Olympic gold medals are actually made mostly of silver.",
+            "You are born with 300 bones; by the time you are an adult you will have 206.",
+            "It takes about 8 minutes for light from the Sun to reach Earth.",
+            "Some bamboo plants can grow almost a meter in just one day.",
+            "The state of Florida is bigger than England.",
+            "Some penguins can leap 2-3 meters out of the water.",
+            "On average, it takes 66 days to form a new habit.",
+            "Mammoths still walked the earth when the Great Pyramid was being built."
+    };
+
+    // Member variable (properties about the object)
+
+    //Method (abilities: things the object can do)
+    public String getFact() {
+
+
+        String fact = "";
+        // Randomly select a fact
+        Random randomGenerator = new Random();  // Construct a new Random number generator
+        int randomNumber = randomGenerator.nextInt(mFacts.length);
+
+        fact = mFacts[randomNumber];
+
+        return fact;
+    }
+}
